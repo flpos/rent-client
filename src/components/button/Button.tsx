16d415dc0e748ad723d2c48d615ac46abc74d34d
Felipe Oliveira <felipe_ods@outlook.com>
2021-09-10 12:11:03 -0300

    refactor: use button from material-ui

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
+import { Button as MUIButton } from '@material-ui/core';
 
 type Props = {
   onClick?: () => void;
   role?: string;
   type?: 'submit' | 'button' | 'reset';
+  color?: 'default' | 'inherit' | 'primary' | 'secondary';
 };
 
-const Button: React.FC<Props> = ({ children, onClick, role, type }) => {
+const Button: React.FC<Props> = ({
+  children,
+  onClick,
+  role,
+  type,
+  color = 'default',
+}) => {
   return (
-    <button onClick={onClick} role={role} type={type}>
+    <MUIButton
+      color={color}
+      variant='contained'
+      onClick={onClick}
+      role={role}
+      type={type}
+    >
       {children}
-    </button>
+    </MUIButton>
   );
 };
 
