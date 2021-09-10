c15dadf5ebd0e1c4dd4d414558427c3c5cc8dc0b
Felipe Oliveira <felipe_ods@outlook.com>
2021-09-10 12:56:19 -0300

    refactor: minimal login page

--- src/pages/login/Login.page.tsx
+++ src/pages/login/Login.page.tsx
@@ -4,6 +4,8 @@ import Input from '../../components/input';
 import { LoginUseCase } from '../../domain/use-cases/login.usecase';
 import { UserPersistenceService } from '../../infra/services/user-persistence.service';
 import { History } from 'history';
+import { Box, Container } from '@material-ui/core';
+import useStyles from './styles';
 
 type Props = {
   loginUseCase: LoginUseCase;
@@ -27,19 +29,23 @@ const LoginPage: React.FC<Props> = ({
     });
   };
 
+  const styles = useStyles();
+
   return (
-    <>
+    <Container className={styles.container}>
       <form onSubmit={handleSubmit}>
-        <Input
-          placeholder='username'
-          onChange={(value) => setUsername(value)}
-        />
-        <Input placeholder='email' onChange={(value) => setEmail(value)} />
-        <Button role='submit' type='submit'>
-          Send
-        </Button>
+        <Box display='flex' flexDirection='column' gridGap='20px'>
+          <Input
+            placeholder='username'
+            onChange={(value) => setUsername(value)}
+          />
+          <Input placeholder='email' onChange={(value) => setEmail(value)} />
+          <Button role='submit' type='submit'>
+            Send
+          </Button>
+        </Box>
       </form>
-    </>
+    </Container>
   );
 };
 
---
+++ src/pages/login/styles.ts
@@ -0,0 +1,12 @@
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  container: {
+    height: '95vh',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default useStyles;
