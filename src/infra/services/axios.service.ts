711864288b597e8b3b6d13a0ca48543939a9113f
Felipe Oliveira <felipe_ods@outlook.com>
2021-09-10 17:05:04 -0300

    feat: change url for production

--- src/infra/services/axios.service.ts
+++ src/infra/services/axios.service.ts
@@ -1,5 +1,10 @@
 import axios from 'axios';
 
-const axiosInstance = axios.create({ baseURL: process.env.REACT_APP_API });
+const axiosInstance = axios.create({
+  baseURL:
+    process.env.NODE_ENV === 'development'
+      ? process.env.REACT_APP_API
+      : '/backend/',
+});
 
 export default axiosInstance;
