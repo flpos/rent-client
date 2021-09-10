9614448f9d6f48f64e70bf01e36861312bdb94a8
Felipe Oliveira <felipe_ods@outlook.com>
2021-09-10 02:13:45 -0300

    feat: add car details entity

--- src/domain/entities/car.entity.ts
+++ src/domain/entities/car.entity.ts
@@ -1,7 +1,12 @@
 export type Car = {
-    id: string,
-    brand: string,
-    model: string,
-    year: number,
-    imageUrl: string,
-  }
+  id: string;
+  brand: string;
+  model: string;
+  year: number;
+  imageUrl: string;
+};
+
+export type CarDetail = Car & {
+  color: string;
+  kilometers: number;
+};
